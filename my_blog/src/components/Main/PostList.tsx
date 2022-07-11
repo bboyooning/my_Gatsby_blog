7360dfb288678a189a863b3d731c01e0261eeefa
bboyooning <bboyooning@gmail.com>
2022-07-11 20:32:38 +0900

    add: 카테고리, 게시글 목록 컴포넌트 구현 및 페이지 반응형 적용

---
+++ my_blog/src/components/Main/PostList.tsx
@@ -0,0 +1,40 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import PostItem from 'components/Main/PostItem'
+
+const POST_ITEM_DATA = {
+  title: 'Post Item Title',
+  date: '2022.07.11.',
+  categories: ['Web', 'Frontend', 'Testing'],
+  summary: 'blah blah blah',
+  thumbnail: 'https://avatars.githubusercontent.com/u/77096364?v=4',
+  link: '<https://www.google.co.kr>',
+}
+
+const PostListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    width: 100%;
+    padding: 50px 20px;
+  }
+`
+
+const PostList: FunctionComponent = function () {
+  return (
+    <PostListWrapper>
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+    </PostListWrapper>
+  )
+}
+
+export default PostList
--- my_blog/src/pages/index.tsx
+++ my_blog/src/pages/index.tsx
@@ -2,12 +2,20 @@ import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from 'components/Common/GlobalStyle'
 import Introduction from 'components/Main/Introduction'
+import CategoryList from 'components/Main/CategoryList'
+import PostList from 'components/Main/PostList'
 import Footer from 'components/Common/Footer'
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+}
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
 `
 
 const IndexPage: FunctionComponent = function () {
@@ -15,6 +23,8 @@ const IndexPage: FunctionComponent = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+      <PostList />
       <Footer />
     </Container>
   )
