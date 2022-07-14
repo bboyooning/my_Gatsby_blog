3213c09c01cc0a2526545754e05b4f61a98f4660
bboyooning <bboyooning@gmail.com>
2022-07-14 20:27:01 +0900

    add: robots.txt 파일 생성

--- my_blog/gatsby-config.js
+++ my_blog/gatsby-config.js
@@ -101,5 +101,11 @@ module.exports = {
       },
     },
     'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
   ],
 }
