22185715866881fc4618556b40ef032af24ed6c7
bboyooning <bboyooning@gmail.com>
2022-07-14 20:50:17 +0900

    modify: 블로그 배포 전 siteUrl 수정

--- my_blog/gatsby-config.js
+++ my_blog/gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `보윤이의 개발 블로그`,
     description: `어제보다 오늘 더 성장하는 개발자 김보윤 입니다.`,
     author: `bboyooning`,
-    siteUrl: 'https://my-website-link.com',
+    siteUrl: 'https:/develop-blog.github.io/',
   },
   plugins: [
     {
@@ -96,7 +96,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://my-website.com/',
+        siteUrl: 'https:/develop-blog.github.io/',
         stripQueryString: true,
       },
     },
